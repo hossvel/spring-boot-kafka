965a2ce5032dc52e7c9dfe1e68fb0babbfdf2828
devhoss <velasco2288@gmail.com>
2023-10-07 09:44:29 -0500

    first commit

---
+++ spring-boot-kafka-producer/src/test/java/com/devhoss/kafka/producer/SpringBootKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhoss.kafka.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
